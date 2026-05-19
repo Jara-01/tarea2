5d6acb4320320c27226ccdd21f6a6a1caccbe61b
Benja <benjaminrrivassepul@gmail.com>
2026-05-19 18:10:26 -0400

    Implementa compareTo en la clase Nota

--- src/main/java/tarea2/Nota.java
+++ src/main/java/tarea2/Nota.java
@@ -1,14 +1,22 @@
 package tarea2;
+import java.time.*;
 
-class Nota {
+class Nota implements Comparable<Nota> {
     private String contenido;
+    private Instant hora;
 
-    public Nota(String texto, Reunion reunion){
+    public Nota(String texto, Reunion reunion) {
         this.contenido = texto;
+        this.hora = Instant.now();
     }
 
     @Override
-    public String toString(){
-        return "Nota: " + this.contenido;
+    public int compareTo(Nota o) {
+        return this.hora.compareTo(o.hora);
+    }
+
+    @Override
+    public String toString() {
+        return "("+ hora +")" + " Nota: " + this.contenido;
     }
 }
